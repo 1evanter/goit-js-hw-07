0e3ca0a2920883e99c537aab1007ee5e69302ad5
1evanter <rita.oleksenkoo@gmail.com>
2023-06-15 12:02:26 +0200

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,52 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery')
+const galleryElements = createGallery(galleryItems);
+
+galleryList.insertAdjacentHTML('afterbegin', galleryElements)
+
+function createGallery (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+ return ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('')
+}
+
+
+galleryList.addEventListener('click', handleModalOpen);
+
+function handleModalOpen(evt) {
+  evt.preventDefault();
+    
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+  createLightbox(evt);
+}
+
+function createLightbox(evt) {
+  const modalImg = evt.target.dataset.source;
+  
+    const instance = basicLightbox.create(`
+    <img src="${modalImg}" width="800" height="600">
+`);
+
+  instance.show();
+
+      window.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape') {
+            instance.close();
+        };
+    }, { once: true });
+}
