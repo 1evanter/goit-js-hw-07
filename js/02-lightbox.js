0b70889202abb361c99daa25980c8180271bb653
1evanter <rita.oleksenkoo@gmail.com>
2023-06-15 12:38:55 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery')
+const galleryElements = createGallery(galleryItems);
+
+galleryList.insertAdjacentHTML('afterbegin', galleryElements)
+
+function createGallery (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+ return ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('')
+}
+
+galleryList.addEventListener('click', handleModalOpen);
+
+function handleModalOpen(evt) {
+    evt.preventDefault();
+
+    if (evt.nodename !== 'IMG') {
+        return;
+    }
+
+    createSimpleLightbox(evt);
+}
+
+createSimpleLightbox();
+
+function createSimpleLightbox() {
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+}
